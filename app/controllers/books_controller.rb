de1b95384068d808eff19498d872bd3a96a952dd
Satchit Shenoy <pixelsandapps@gmail.com>
2017-03-27 21:51:16 +0200

    Removed whitespace from books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-
 	before_action :set_book, only: [:show, :edit, :update, :destroy]
 
 
@@ -57,5 +56,4 @@ class BooksController < ApplicationController
 	def set_book
 		@book = Book.find(params[:id])
 	end
-
 end
