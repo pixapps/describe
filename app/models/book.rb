06bb87366848ea460c1408f78f286a6eac684952
Satchit Shenoy <pixelsandapps@gmail.com>
2017-03-27 19:07:41 +0200

    Added validations for length of book description with tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
 	# validations
 	validates_presence_of :name, :description
 	validates_length_of :name, minimum: 3
+	validates_length_of :description, minimum: 10
 
 
 	def to_s
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Book, type: :model do
 		it { is_expected.to validate_presence_of(:name) }
 		it { is_expected.to validate_presence_of(:description) }
 		it { should validate_length_of(:name).is_at_least(3) }
+		it { should validate_length_of(:description).is_at_least(10) }
 
 	end
 
