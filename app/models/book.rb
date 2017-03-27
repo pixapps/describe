9b33daa210ba274ec34ae9d6478c3d5d3a0441ad
Satchit Shenoy <pixelsandapps@gmail.com>
2017-03-27 21:50:17 +0200

    Removed whitespace from book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,8 @@
 class Book < ApplicationRecord
-
 	# validations
 	validates_presence_of :name, :description
 	validates_length_of :name, minimum: 3
 	validates_length_of :description, minimum: 10
-
 
 	def to_s
 		name
